0ad1fa58a7a8a3edb5f9c6ef23ee0406e3aba970
Lue <lue.amaral@etec.sp.gov.br>
2024-03-21 17:43:40 -0300

    bottom bar more routes

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,36 +1,64 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
-import { Provider } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../screens/Home';
 import Next from '../screens/Next';
 import Search from '../screens/Search';
+import Notifications from '../screens/Notifications';
+import Profile from '../screens/Profile';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export const BottomNavigation = () => {
-    return (
-      <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Next}
-          options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="cog" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-    );
-  }
+  return (
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons  name="home" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Next"
+        component={Next}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="fire" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={24} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+  );
+}
